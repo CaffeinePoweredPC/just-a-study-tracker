e7c740d289cac5abbf4455135bbfb117d7e996aa
CaffeinePoweredPC <thechillestoof@gmail.com>
2026-05-04 20:20:13 +0800

    update logic + add controller module (planned)

--- src/logic.py
+++ src/logic.py
@@ -4,41 +4,104 @@ from typing import Optional
 import ctypes
 import json
 
-class Session:
-    def __init__(self, subject: str, duration: float) -> None:
+
+#REMINDER: CONTROLLER CONTROLS TIMING, NONE OF THE BELOW SHOULD
+
+class Subject:
+    def __init__(self, subject: str, desc: Optional[str], tags: Optional[list[str]]) -> None:
         if type(subject) != str:
             raise TypeError("invalid subject")
+
         self.subject = subject
+        self.description = desc
+        self.tags = tags
 
-        if type(duration) != float:
-            raise TypeError("invalid duration")
+    def jsonify(self):
+        converted = {
+            "subject": self.subject,
+            "description": self.description,
+            "tags": self.tags
+        }
+        return converted
+
+    def un_jsonify(self):
+
+class SubjectManager: #container for subject objects
+    def __init__(self):
+        self.subject_list = []
+
+    def add_subject(self, subject: object):
+        self.subject_list.append(subject)
+
+    def remove_subject(self,subject:object):
+        if subject not in self.subject_list:
+            raise IndexError("subject not found")
+        self.subject_list.remove(subject)
+
+    def convert_to_json(self) -> list[dict[str,str]]:
+
+
+#Session & SessionTracker below serve only to contain current / past sessions
+class Session:
+    def __init__(self, subject: str)-> None:
+        if type(subject) != str:
+            raise TypeError("invalid subject")
+
+        self.subject = subject
+        self.start_time = None
+        self.end_time = None
+        self.duration = None
+        self.is_active = False
+
+    def start_sesh(self,start_time: float=None):
+        self.is_active = True
+        self.start_time = start_time
+
+    def end_sesh(self,time_now: float=None) -> None:
+        #time_now passed from controller
+        if not self.is_active:
+            pass
         else:
-            if duration <= 0.0:
-                raise ValueError("invalid duration")
-        self.duration = duration
+            if time_now is None:
+                time_now = time.time()
 
-    def timer(self):
-        pass
+            self.duration = time_now - self.start_time
+            self.end_time = time_now
+            self.is_active = False
 
-    def end_session(self):
-        pass
-
+    def jsonify(self):
+        converted = {
+            "subject": self.subject,
+            "start_time": self.start_time,
+            "end_time": self.end_time,
+            "duration": self.duration
+        }
+        return converted
 
 class SessionTracker:
     def __init__(self) -> None:
+        self.active_session = None
         self.session_list = []
+
+    def start_session(self,subject: str):
+        self.active_session = Session(subject)
+        self.active_session.start_sesh(time.time())
+
+    def end_session(self):
+        self.active_session.end_sesh(time.time())
+        self.log_session(self.active_session)
+        self.active_session = None
 
     def log_session(self,session: object) -> None:
         self.session_list.append(session)
 
     def remove_session(self, session: object) -> None:
-        #UI will pass exact session as arg
         if session not in self.session_list:
             raise IndexError("session not found")
         self.session_list.remove(session)
 
 
-    def total_study_time(self):
+    def total_study_time(self) -> None:
         #counts all time, not yet accounting for daily / weekly, will implement in the future
         total = 0
         for session in self.session_list:
@@ -46,8 +109,20 @@ class SessionTracker:
         return total
 
 
+#FIX this
 class StorageHandler:
     def __init__(self) -> None:
+        self.file_path = None
+
+    def set_file_path(self,path):
+        if type(path) != str:
+            raise TypeError("Invalid path")
+        self.file_path = path
+
+    def read_json(self):
+        pass
+
+    def write_json(self):
         pass
 
 
@@ -55,6 +130,7 @@ class StorageHandler:
 class FocusMonitor(QObject):
 
     warning_signalled = Signal()
+    #will link to controller
 
     def __init__(self):
         super().__init__()
@@ -80,6 +156,7 @@ class FocusMonitor(QObject):
     def unfocus_timer(self):
         time_now = time.perf_counter()
         cur_win = self.current_window()
+
         if cur_win in self.blacklist:  #returns False if None, intentional
             if self.unfocus_start is None:
                 self.unfocus_start = time_now
--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
- 
+
+from logic import SessionTracker
+
+exit = 0
+
+
+
+if __name__ == "__main__":
+
+
+#just random test code for basic logic
+
+
+
